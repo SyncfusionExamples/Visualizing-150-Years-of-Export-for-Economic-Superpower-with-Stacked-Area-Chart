83a2c15e54830f8f9fbb52c884cecd7831638fa3
NitheeshKumarThangaraj <nitheeshkumar.t@syncfusion.com>
2024-07-12 16:39:33 +0530

    Updated the code

--- StackedAreaBlog/MainPage.xaml.cs
+++ StackedAreaBlog/MainPage.xaml.cs
@@ -10,13 +10,9 @@ namespace StackedAreaBlog
             InitializeComponent();
 #if WINDOWS || MACCATALYST
             textAnnotation.Text = "In the late 1800s, the UK was the world's \nlargest exporter by value. By 2022, it had \nshrunk to the 15th largest.";
-#endif
 
-#if WINDOWS || MACCATALYST
             japanText.Text = "Japan's growth in electronic goods production \nbriefly made it one of the world's largest\nexporters.";
-#endif
 
-#if WINDOWS || MACCATALYST
             titleText.Text = "100+ Years of Global Trade: Economic Superpowers";
 #else
             titleText.Text = "100+ Years of Global Trade";
