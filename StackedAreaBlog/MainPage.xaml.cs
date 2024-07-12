70ff8296dc43a6b07fb20a88834a91eb83ed309a
NitheeshKumarThangaraj <nitheeshkumar.t@syncfusion.com>
2024-07-12 18:58:34 +0530

    updated the sample

---
+++ StackedAreaBlog/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Syncfusion.Maui.Charts;
+using System.Globalization;
+
+namespace StackedAreaBlog
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+#if WINDOWS || MACCATALYST
+            textAnnotation.Text = "In the late 1800s, the UK was the world's \nlargest exporter by value. By 2022, it had \nshrunk to the 15th largest.";
+
+            japanText.Text = "Japan's growth in electronic goods production \nbriefly made it one of the world's largest\nexporters.";
+
+            titleText.Text = "100+ Years of Global Trade: Economic Superpowers";
+#else
+            titleText.Text = "100+ Years of Global Trade";
+#endif
+        }
+    }
+
+}
+
---
+++ StackedAreaBlog/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace StackedAreaBlog
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
